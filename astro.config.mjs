05ace5c59bc9d306e942391d42b7c92b76fc52df
git-n-guts <vutienbg730@gmail.com>
2026-04-24 15:53:25 +0700

    Comment out site to fix auth cookie loop on worker domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 
 export default defineConfig({
-  site: 'https://admin.pholio.click',
+  // site: 'https://admin.pholio.click',
   output: 'server',
   adapter: cloudflare(),
   integrations: [react(), keystatic()],
